3a3ecf5a1595c18730bda8c026a464938e726390
GottJagger <gott.chaki@gmail.com>
2023-04-20 17:38:26 -0500

    se hizo una optimizacion a la funcion

--- src/main/scala/httpRequest.scala
+++ src/main/scala/httpRequest.scala
@@ -25,55 +25,31 @@ object httpRequest{
 
   }
   // Funcion post para realizar el request al servidor(API)
-  def postToApi(url: String, message: String, inputDf: DataFrame): Unit = {
-    // Convierte el DataFrame de entrada en un JSON
-    val jsonInput = inputDf.toJSON.collect.mkString("[", ",", "]")
-    val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
-    val jsonObject = mapper.readTree(jsonInput)
 
-    // Realiza una solicitud POST a la API con el JSON como cuerpo
-    val response: HttpResponse[String] = Http(url)
-      .header("Content-Type", "application/json")
-      .postData(jsonObject.toString)
-      .param("message", message)
-      .method("POST")
-      .asString
-    println("Metodo POST request enviada del cliente")
-
-
-    // Imprime la respuesta del servidor
-    println(response.body)
-
-  }
-
-
-  def postToApi1(url: String, inputDf: DataFrame, inputjson: String): Unit = {
-    var jsonDf = ""
-    var jsonMetricas = ""
-    val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
-    var DataJson =""
-    if (inputjson == null) {
-      // Si inputjson es nulo, convierte inputDf a JSON
-      jsonDf = inputDf.toJSON.collect.mkString("[", ",", "]")
-      val DataJson = mapper.readTree(jsonDf)
-    } else if(inputDf==null) {
-      // Si inputjson no es nulo, utiliza su valor
-      jsonMetricas = inputjson
-      val DataJson = mapper.readTree(jsonMetricas)
-    }else if(inputjson!=null && inputDf!=null){
-      jsonDf = inputDf.toJSON.collect.mkString("[", ",", "]")
-      jsonMetricas = inputjson
-      val jsonObjectDf = mapper.readTree(jsonDf)
-      val jsonObjectMetricas = mapper.readTree(inputjson)
-      val jsonNode = mapper.createObjectNode()
-
-      jsonNode.set("jsonMetricas", jsonObjectMetricas)
-      jsonNode.set("jsonDf", jsonObjectDf)
-      DataJson=jsonNode.toString
-    }else {
-      // Si ambos inputjson e inputDf son nulos, muestra un mensaje de error y sale del método
+  def postToApi(url: String, inputDf: DataFrame, inputjson: String): Unit = {
+    val DataJson = if (inputjson != null) {
+      // Si se proporciona inputjson, lo convierte a un árbol JSON
+      new ObjectMapper().registerModule(DefaultScalaModule).readTree(inputjson)
+    } else if (inputDf != null) {
+      // Si se proporciona inputDf, lo convierte a JSON y lo convierte en un árbol JSON
+      val jsonDf = inputDf.toJSON.collect.mkString("[", ",", "]")
+      new ObjectMapper().registerModule(DefaultScalaModule).readTree(jsonDf)
+    } else {
+      // Si ambos son nulos, muestra un mensaje de error y sale del método
       println("Error: No se proporcionó ningún dato para enviar.")
       return
+    }
+
+    // Si se proporciona inputDf, crea un nodo JSON adicional para el JSON del DataFrame
+    if (inputDf != null) {
+      val jsonDf = inputDf.toJSON.collect.mkString("[", ",", "]")
+      val jsonObjectDf = new ObjectMapper().registerModule(DefaultScalaModule).readTree(jsonDf)
+      val jsonNode = new ObjectMapper().createObjectNode()
+      jsonNode.set("jsonMetricas", DataJson)
+      jsonNode.set("jsonDf", jsonObjectDf)
+      jsonNode.toString
+    } else {
+      DataJson.toString
     }
 
     // Realiza una solicitud POST a la API con el JSON como cuerpo
